35541d65e3ee30f0d462fcd690a112a32012a9dc
Xinyi Liu <xliu62@cs.washington.edu>
2018-04-26 03:37:01 -0700

    updated title screen

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -2,17 +2,33 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
+	private var _txtTitle:FlxText;
 	private var _btnPlay:FlxButton;
+	private var _btnLevel:FlxButton;
+	private var _lblColor:FlxColor = FlxColor.WHITE;
 	
 	override public function create():Void
 	{
-		_btnPlay = new FlxButton(0, 0, "Play", clickPlay);
+		_txtTitle = new FlxText(20, 20, 0, "Too Many Blocks", 32);
+		_txtTitle.alignment = CENTER;
+		_txtTitle.screenCenter(X);
+		add(_txtTitle);
+		
+		_btnPlay = new FlxButton(0, FlxG.height/3, "Play", clickPlay);
+		_btnPlay.loadGraphic("assets/images/blocks_for_button.png", true, 50, 65);
+		_btnPlay.label.setFormat("", 10, _lblColor, CENTER);
 		add(_btnPlay);
-		_btnPlay.screenCenter();
+		_btnPlay.screenCenter(X);
+		
+		_btnLevel = new FlxButton(0, FlxG.height/3 + 75, "Level", clickLevel);
+		add(_btnLevel);
+		_btnLevel.screenCenter(X);
 		Data.resetBestTimes();
 		super.create();
 	}
@@ -25,5 +41,10 @@ class MenuState extends FlxState
 	private function clickPlay():Void
 	{
 		FlxG.switchState(new PlayState());
+	}
+	
+	private function clickLevel():Void
+	{
+		//go to level screen
 	}
 }
