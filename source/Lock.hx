3769c21f01d31e649dab985bcbc211e91c102122
Add Gritman <addg@cs.washington.edu>
2018-04-27 17:04:00 -0700

    Added art for Keys/Locks, also created the basic classes for them. Will finish the rest tomorrow

---
+++ source/Lock.hx
@@ -0,0 +1,31 @@
+package;
+
+import flixel.FlxSprite;
+
+
+class Lock extends FlxSprite 
+{
+	
+	// Color:
+	// 0 = Yellow
+	// 1 = Green
+	// 2 = Red
+	// 3 = Blue
+	
+	public var thisColor:Int;
+	
+	public function new(?X:Float=0, ?Y:Float=0, color:Int) 
+	{
+		super(X, Y);
+		loadGraphic(AssetPaths.Locks__png, true, 25, 25);
+		animation.add("type", [color], 6, false);
+		animation.play("type");
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);
+	}
+	
+	
+}
