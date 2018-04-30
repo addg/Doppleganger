33e63abe7b9703880d3016466910060c4e5399b7
Xinyi Liu <xliu62@cs.washington.edu>
2018-04-30 00:03:04 -0700

    display of best time on level select

--- source/LevelState.hx
+++ source/LevelState.hx
@@ -9,10 +9,11 @@ class LevelState extends FlxState
 	private var _btnLevel:FlxButton;
 	private var _txtLevel:FlxText;
 	private var _txtCannotPlay:FlxText;
+	private var _txtBestTime:FlxText;
 	private var _btnMenu:FlxButton;
 	
 	private var offset = 50;
-	private var lvl_per_row = 5; // change this when the buttons are pushed out of the screen
+	private var lvl_per_row = 6; // change this when the buttons are pushed out of the screen
 	
 	override public function create():Void 
 	{
@@ -22,12 +23,16 @@ class LevelState extends FlxState
 		
 		for (i in 0...Data.amtLevels) {
 			if (Data.canPlayLevel[i + 1]) {
-				_btnLevel = new FlxButton(offset * (i % lvl_per_row + 4), offset * (Std.int(i / lvl_per_row) + 2),  i + 1 + "", clickLevel.bind(i+1));
+				_btnLevel = new FlxButton(offset * (i % lvl_per_row + 4), offset * (Std.int(i / lvl_per_row) * 1.25 + 2),  i + 1 + "", clickLevel.bind(i+1));
 				_btnLevel.scale.set(0.5, 2);
 				_btnLevel.label.setFormat("", 15);
 				add(_btnLevel);
+				if (Data.completedLevel[i + 1]) {
+					_txtBestTime = new FlxText(_btnLevel.x + 20, _btnLevel.y + 30, offset, Data.bestTimes[i + 1] + "s", 10);
+					add(_txtBestTime);
+				}
 			} else {// FlxText does not align with FlxButton, so the offset is hardcoded
-				_txtCannotPlay = new FlxText(offset * (i % lvl_per_row + 4.5) + 5, offset * (Std.int(i / lvl_per_row) + 2) + 5, offset, i + 1 + "", 15);
+				_txtCannotPlay = new FlxText(offset * (i % lvl_per_row + 4.5) + 5, offset * (Std.int(i / lvl_per_row) * 1.25 + 2) + 5, offset, i + 1 + "", 15);
 				add(_txtCannotPlay);
 			}
 		}
