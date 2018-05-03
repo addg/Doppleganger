47fbf7b91ef1db4d472031cbd3317b852553ec04
Xinyi Liu <xliu62@cs.washington.edu>
2018-05-03 01:51:03 -0700

    menu & level select background

--- source/LevelState.hx
+++ source/LevelState.hx
@@ -3,36 +3,61 @@ import flixel.FlxG;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+
 
 class LevelState extends FlxState
 {
+	private var _backgroundColor:FlxSprite;
+	private var _backgroundBlock:FlxSprite;
+	private var _background:FlxSprite;
 	private var _btnLevel:FlxButton;
+	private var _btnSprite:FlxSprite;
 	private var _txtLevel:FlxText;
 	private var _txtCannotPlay:FlxText;
+	private var _txtSprite:FlxSprite;
 	private var _txtBestTime:FlxText;
 	private var _btnMenu:FlxButton;
+	private var _lblColor:FlxColor = FlxColor.BLACK;
 	
 	private var offset = 50;
 	private var lvl_per_row = 6; // change this when the buttons are pushed out of the screen
 	
 	override public function create():Void 
 	{
+		_backgroundColor = new FlxSprite();
+		_backgroundColor.loadGraphic(AssetPaths.bg_color__jpg);
+		add(_backgroundColor);
+		
+		_backgroundBlock = new FlxSprite();
+		_backgroundBlock.loadGraphic(AssetPaths.level_bg__png);
+		add(_backgroundBlock);
+		
 		_txtLevel = new FlxText(0, 0, 0, "Choose A Level", 35);
+		_txtLevel.color = _lblColor;
 		_txtLevel.screenCenter(X);
 		add(_txtLevel);
 		
 		for (i in 0...Data.amtLevels) {
 			if (Data.canPlayLevel[i + 1]) {
-				_btnLevel = new FlxButton(offset * (i % lvl_per_row + 4), offset * (Std.int(i / lvl_per_row) * 1.25 + 2),  i + 1 + "", clickLevel.bind(i+1));
-				_btnLevel.scale.set(0.5, 2);
-				_btnLevel.label.setFormat("", 15);
+				_btnLevel = new FlxButton(offset * (i % lvl_per_row * 1.25 + 3), offset * (Std.int(i / lvl_per_row) * 1.25 + 2),  i + 1 + "", clickLevel.bind(i+1));
+				_btnLevel.loadGraphic(AssetPaths.btn_white__png, true, 50, 50);
+				_btnLevel.label.setFormat(_txtLevel.font, 15, _lblColor, FlxTextAlign.CENTER);
 				add(_btnLevel);
 				if (Data.completedLevel[i + 1]) {
-					_txtBestTime = new FlxText(_btnLevel.x + 20, _btnLevel.y + 30, offset, Data.bestTimes[i + 1] + "s", 10);
+					_txtBestTime = new FlxText(_btnLevel.x, _btnLevel.y + _btnLevel.height * 0.6, offset, Data.bestTimes[i + 1] + "s", 10);
+					_txtBestTime.color = _lblColor;
+					_txtBestTime.alignment = "center";
 					add(_txtBestTime);
 				}
-			} else {// FlxText does not align with FlxButton, so the offset is hardcoded
-				_txtCannotPlay = new FlxText(offset * (i % lvl_per_row + 4.5) + 5, offset * (Std.int(i / lvl_per_row) * 1.25 + 2) + 5, offset, i + 1 + "", 15);
+			} else {
+				_btnSprite = new FlxSprite(offset * (i % lvl_per_row * 1.25 + 3), offset * (Std.int(i / lvl_per_row) * 1.25 + 2));
+				_btnSprite.loadGraphic(AssetPaths.btn_black__png);
+				add(_btnSprite);
+				//_txtCannotPlay = new FlxText(offset * (i % lvl_per_row * 1.25 + 3.25) + 5, offset * (Std.int(i / lvl_per_row) * 1.25 + 2) + 5, offset, i + 1 + "", 15);
+				_txtCannotPlay = new FlxText(offset * (i % lvl_per_row * 1.25 + 3), offset * (Std.int(i / lvl_per_row) * 1.25 + 2), offset, i + 1 + "", 15);
+				_txtCannotPlay.alignment = "center";
 				add(_txtCannotPlay);
 			}
 		}
--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,6 +9,8 @@ import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
+	private var _backgroundColor:FlxSprite;
+	private var _backgroundBlock:FlxSprite;
 	private var _txtTitle:FlxText;
 	private var _txtSubtitle:FlxText;
 	private var _txtRestart:FlxText;
@@ -18,7 +20,7 @@ class MenuState extends FlxState
 	private var _sprRight:FlxSprite;
 	private var _btnPlay:FlxButton;
 	private var _btnLevel:FlxButton;
-	private var _lblColor:FlxColor = FlxColor.WHITE;
+	private var _lblColor:FlxColor = FlxColor.BLACK;
 	
 	private var offset = 50; // same as the pixel size of the square
 	private var font_size = 35;
@@ -34,15 +36,24 @@ class MenuState extends FlxState
 		//FlxG.mouse.enabled = true;
 		FlxG.mouse.visible = true;
 		
+		_backgroundColor = new FlxSprite();
+		_backgroundColor.loadGraphic(AssetPaths.bg_color__jpg);
+		add(_backgroundColor);
+		
+		_backgroundBlock = new FlxSprite();
+		_backgroundBlock.loadGraphic(AssetPaths.menu_bg__png);
+		add(_backgroundBlock);
+		
 		_txtTitle = new FlxText(20, 20, 0, "Too Many Blocks!", font_size);
+		_txtTitle.color = _lblColor;
 		_txtTitle.alignment = CENTER;
 		_txtTitle.screenCenter(X);
 		add(_txtTitle);
 		
 		// buttons
 		_btnPlay = new FlxButton(0, FlxG.height/3, "Play", clickPlay);
-		_btnPlay.loadGraphic("assets/images/blocks_for_button_long.png", true, 150, 50);
-		_btnPlay.label.setFormat("", font_size - 10, _lblColor, CENTER);
+		_btnPlay.loadGraphic(AssetPaths.btn_play__png, true, 150, 50);
+		_btnPlay.label.setFormat(_txtTitle.font, font_size - 10, _lblColor, CENTER);
 		_btnPlay.label.offset.x += offset;
 		add(_btnPlay);
 		_btnPlay.screenCenter(X);
@@ -54,17 +65,20 @@ class MenuState extends FlxState
 		
 		// tips for player
 		_txtSubtitle = new FlxText(20, 20 + offset, 0, "Join the Blocks!", font_size - 10);
+		_txtSubtitle.color = _lblColor;
 		_txtSubtitle.screenCenter(X);
 		add(_txtSubtitle);
 		
 		_txtRestart = new FlxText(20, 20 + offset * 7, 0, "Press R to Restart Level", font_size - 20);
+		_txtRestart.color = _lblColor;
 		_txtRestart.screenCenter(X);
 		add(_txtRestart);
 		
 		_txtControls = new FlxText(_btnPlay.x - offset * 0.25, 20 + offset * 4.5, 0, "Jump: \nMove: ", font_size - 15);
-		_sprUp = new FlxSprite(_btnPlay.x + offset * 2.25, 20 + offset * 4.5, "assets/images/up_arrow.png");
-		_sprLeft = new FlxSprite(_btnPlay.x + offset * 2, 20 + offset * 5.0, "assets/images/left_arrow.png");
-		_sprRight = new FlxSprite(_btnPlay.x + offset * 2.5, 20 + offset * 5.0, "assets/images/right_arrow.png");
+		_txtControls.color = _lblColor;
+		_sprUp = new FlxSprite(_btnPlay.x + offset * 2.25, 20 + offset * 4.5, AssetPaths.up_arrow__png);
+		_sprLeft = new FlxSprite(_btnPlay.x + offset * 2, 20 + offset * 5.0, AssetPaths.left_arrow__png);
+		_sprRight = new FlxSprite(_btnPlay.x + offset * 2.5, 20 + offset * 5.0, AssetPaths.right_arrow__png);
 		add(_txtControls);
 		add(_sprUp);
 		add(_sprLeft);
